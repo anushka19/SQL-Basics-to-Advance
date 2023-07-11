ce2d33392962f2b939b359061af52dd9c20d43fd
ANUSHKA BOMMAKANTY <anushkabommakanty@gmail.com>
2023-07-11 23:18:27 +0530

    sql insert

--- Section6-Database-Creation/Insert-Statement/insert.sql
+++ Section6-Database-Creation/Insert-Statement/insert.sql
@@ -1,4 +1,19 @@
 Insert into movies
 values (141,"Bahubali 3", "Bollywood", 2030, 9.0, "Arka Media Works",2);
 
+Insert into movies
+(title, industry, release_year)
+values (DEFAULT,"Bahubali 5", "Bollywood", 2035,NULL, 2);
+
+Insert into movies
+values (DEFAULT,"Bahubali 5", "Bollywood", 2037,NULL,NULL, 2);
+
+Insert into movies 
+(title, industry, language_id)
+values
+("Inception 2", "Hollywood", 5),
+("Inception 3", "Hollywood", 5),
+("Inception 4", "Hollywood", 5),
+("Inception 5", "Hollywood", 5);
+
 SELECT * FROM movies;
